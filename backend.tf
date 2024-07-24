f71eb16b11b51d044bf24f1d262d0bee980e5ac0
johntoby <johntoby007@gmail.com>
2024-07-24 19:35:59 +0100

    update to backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "jt-kcvpc-bucket"
     key            = "terraform-state-dir/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     profile        = "jtbaba"
     dynamodb_table = "dynamo-table-kcvpc"
   }
